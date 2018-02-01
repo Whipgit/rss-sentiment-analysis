af7954f091cbde22ca87b807de99b5dc9d9d12ab
Stijn Winand <whiplash174@gmail.com>
2018-02-01 17:30:23 -0500

    Update rss-sentiment-analysis.js

--- rss-sentiment-analysis.js
+++ rss-sentiment-analysis.js
@@ -1,6 +1,18 @@
-/**
-* @param context {WebtaskContext}
-*/
-module.exports = function(context, cb) {
-  cb(null, { hello: context.query.name || 'Anonymous' });
+'use latest';
+const parser = require('rss-parser-browser');
+const sentiment = require('sentiment');
+
+module.exports = function (ctx, done) {
+  if (!ctx || !ctx.data || !ctx.data.url) {
+    done('Please provide a valid RSS URL in the url parameter');
+  }
+  parser.parseURL(ctx.data.url, (err, parsed) => {
+    if (err) done('Please provide a valid RSS URL in the url parameter');
+    parsed.feed.entries.map(item => {
+      item.analysis = sentiment(item.content);
+      delete item.analysis.tokens;
+      delete item.analysis.words;
+    })
+    done(null, { items: parsed.feed.entries });
+  })
 };
